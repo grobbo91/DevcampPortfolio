22cf6b0cd2f5084af1bc0b2c2af8869c179c3659
Grant Robertson <Grant@Grants-MacBook-Pro.local>
2018-08-13 21:24:01 +1000

    Implemented full petergate for portfolio items and blogs. authorised site_admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
 	include SetSource
 	include CurrentUserConcern
 	include DefaultPageContent
-	
+
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = RobertsonViewTool::Renderer.copyright 'Grant Robertson', 'all rights reserved'
+	end
+end
+
+module RobertsonViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
 
--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
+access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
   # GET /blogs
   # GET /blogs.json
   def index
@@ -32,10 +34,9 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-        format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
+        
       end
     end
   end
